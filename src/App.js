966f0d733897896a10ff1109d4de720c8ed5becc
novello99 <novellosal@gmail.com>
2022-04-04 20:49:49 -0500

    Setting up Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Palindrome from "./Palindrome";
 
 function App() {
+  const [palindrome, setPalindrome] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Enter Phrase Below</div>
+      <input type={"text"} />
+      <button>Check for Palindrome</button>
+      <Palindrome palindrome={palindrome} setPalindrome={setPalindrome} />
+    </>
   );
 }
 
---
+++ src/Palindrome.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Palindrome(palindrome, setPalindrome) {
+  return null;
+}
